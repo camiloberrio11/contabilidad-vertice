622d522e4853e29fbde8881075e2d4cd36963599
Juan Camilo Berrio <camilo.berrio@metis.com.co>
2023-02-06 11:19:42 -0500

    se limpia el app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,49 +32,12 @@ FullCalendarModule.registerPlugins([
 
 import { MetismenuAngularModule } from '@metismenu/angular';
 import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
-import { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
-import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
 
 import { NgxDropzoneModule } from 'ngx-dropzone';
 import { CarouselModule } from 'ngx-owl-carousel-o';
 
-import { MatListModule } from '@angular/material/list';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatIconModule } from '@angular/material/icon';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatBadgeModule } from '@angular/material/badge';
-import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
-import { MatButtonModule } from '@angular/material/button';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatCardModule } from '@angular/material/card';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatRadioModule } from '@angular/material/radio';
-import { DragDropModule } from '@angular/cdk/drag-drop';
-import { MatChipsModule } from '@angular/material/chips';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatSelectModule } from '@angular/material/select';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatRippleModule } from '@angular/material/core';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatSortModule } from '@angular/material/sort';
-import { MatStepperModule } from '@angular/material/stepper';
-import { MatTableModule } from '@angular/material/table';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatTreeModule } from '@angular/material/tree';
 
 /* #########################  SITE PAGES COMPONENT ###################*/
 
@@ -137,43 +100,10 @@ import { CargarArchivoComponent } from './auth/procesamiento/cargar-archivo/carg
     NgxDropzoneModule,
     CarouselModule,
 
-    MatListModule,
-    MatAutocompleteModule,
-    MatFormFieldModule,
-    MatInputModule,
+
     MatSlideToggleModule,
     MatIconModule,
-    MatTabsModule,
-    MatBadgeModule,
-    MatBottomSheetModule,
-    MatButtonModule,
-    MatButtonToggleModule,
-    MatCardModule,
-    MatCheckboxModule,
-    MatRadioModule,
-    DragDropModule,
-    MatChipsModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatDialogModule,
-    MatToolbarModule,
-    MatMenuModule,
-    MatDividerModule,
-    MatExpansionModule,
-    MatSelectModule,
-    MatGridListModule,
-    MatPaginatorModule,
-    MatProgressBarModule,
-    MatSliderModule,
-    MatProgressSpinnerModule,
-    MatRippleModule,
-    MatSidenavModule,
-    MatSnackBarModule,
-    MatSortModule,
-    MatStepperModule,
-    MatTableModule,
-    MatTooltipModule,
-    MatTreeModule,
+
   ],
   providers: [SharedService],
   bootstrap: [AppComponent],
